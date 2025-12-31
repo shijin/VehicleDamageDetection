5567ad64a389bc5c812194aeaf367957155684ea
Shijin <kshijin92@gmail.com>
2025-12-31 09:08:26 +0530

    Initial commit: Vehicle Damage Prediction App

---
+++ streamlit/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from model_helper import predict
+import os
+
+st.write("Current working directory:", os.getcwd())
+st.title('Vehicle Damage Prediction App')
+
+uploaded_file = st.file_uploader("Upload the file", type=["jpg", "png"])
+
+if uploaded_file:
+    image_path = "temp_file.jpg"
+    with open(image_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+        st.image(uploaded_file, caption='Uploaded File', use_container_width=True)
+        prediction = predict(image_path)
+        st.info(f'Predicted Class: {prediction}')
